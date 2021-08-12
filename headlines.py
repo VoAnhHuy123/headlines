6ae0a28c2cc1c7bb4bb20fc0e98c002866bbf349
huyvo123 <huyvo2581999@gmail.com>
2021-08-12 15:06:56 +0700

    POST requests

--- headlines.py
+++ headlines.py
@@ -13,10 +13,10 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             'vnexpress': 'https://vnexpress.net/rss/tin-moi-nhat.rss'}
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 # @app.route('/<publication>')
 def get_news():
-    query = request.args.get('publication')
+    query = request.form.get('publication')
     if not query or query.lower() not in RSS_FEEDS:
         query = 'vnexpress'
 
